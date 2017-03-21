810285b2a4e40e6dc660e8bb543d8faba37f0c2e
Jonathan Sanford <jonathan.l.sanford@wmich.edu>
2017-03-21 11:43:06 -0400

    Initial Checkin

---
+++ CS1120SanfordHirschLA5/src/edu/wmich/cs1120/la5/HighArea.java
@@ -0,0 +1,20 @@
+/************************************************
+ * @author : Jonathan Sanford
+ * @author : Chad Hirsch
+ * Class: CS II - 1120 - Thur - 2:30pm
+ * Lab 5 - Mars Rover 2.0
+ * Due 03/30/2017 - 11:59pm
+ * **********************************************
+ * Program Purpose: Simulates a rover on Mars.
+ * Limited user inputs.
+ * **********************************************
+ * Class: XXXXXXXX
+ * Purpose: XXXXXXXXXXXXX
+ * **********************************************
+ */
+
+package edu.wmich.cs1120.la5;
+
+public class HighArea extends Area {
+
+}
